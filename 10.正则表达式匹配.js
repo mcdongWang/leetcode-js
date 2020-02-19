deacb7b7f6879f1193fd0fe966eb5a39eba8238b
mcdong <363534569@qq.com>
2020-02-19 09:27:06 +0800

    10 coding

--- 10.正则表达式匹配.js
+++ 10.正则表达式匹配.js
@@ -17,7 +17,8 @@ var isMatch = function(s, p) {
 
     // 判断相等  a == a  || a == .
     function check(si, pi){
-        return s[si] == p[pi] || p[pi] == '.'
+        // si pi 代表第几个数字 不涉及0 开头的问题
+        return s[si - 1] == p[pi - 1] || p[pi - 1] == '.'
     }
 
     // dp初始化
@@ -30,25 +31,41 @@ var isMatch = function(s, p) {
                 // dp[0][0] 代表空字符串和空正则 所以为true
                 dp[i][j] = true
                 continue
-            }else if(i = ){
-                
+            }else if(j == 0){
+                // 正则为空 则一定为false
+                dp[i][j] = false                
             }else if(i == 0 && j >= 2){
-                //
-                dp[i][j] = p[j - 1] == '*' && dp[0][j - 2]
-            }else if(){
-
+                // 如果 X* 组合的连续 则 可匹配空字符串
+                 dp[i][j] = p[j - 1] == '*' && dp[i][j - 2]
+            }else if(i == 0 && j == 1){
+                // 处理 dp[0][1]
+                dp[i][j] = false
             }else{
                 // 状态转移方程 分类讨论
                 if(p[j - 1] == '*'){
-                    // 如果当前正则字符是 *
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j - 1] && check(i - 1, j - 2))
+                    
+                    
+                    // 当前正则字符是 *
+                    // dp[i][j] = 
+                    //     // a/a => a/ab*
+                    //     dp[i][j - 2]
+                    //     // 
+                    //     || dp[i][j - 1]
+                    //     // a/ab* => aa/ab*a*
+                    //     || (dp[i - 1][j - 2] && check(i, j - 1)) 
+                    //     // a/a => aa/a*
+                    //     || dp[i - 1][j - 1] && check(i, j - 1)
+                    //     // ' '/.* => ... =>  aaa/.*
+                    //     || dp[i - 1][j] && s[i - 1] == s[i - 2] 
                 }else{
-                    dp[i][j] = dp[i - 1][j - 1] && check(i - 1, j - 1)
+                    // 当前正则字符不是 * 
+                    dp[i][j] = dp[i - 1][j - 1] && check(i, j)
                 }
             }
 
         }
     }
+    console.log(dp)
     return !!dp[sl][pl]
 };
 // @lc code=end
